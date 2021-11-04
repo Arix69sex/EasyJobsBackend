70f398c9a27961a4f5ce9bbc05fe0807a4b07020
Arix69sex <arix69sex@gmail.com>
2021-11-04 02:06:06 -0500

    Unit And Integrations test

--- src/test/java/com/easyjobs/integration/UserIntegrationTest.java
+++ src/test/java/com/easyjobs/integration/UserIntegrationTest.java
@@ -2,17 +2,12 @@ package com.easyjobs.integration;
 
 import com.easyjobs.domain.model.User;
 import com.easyjobs.domain.repository.UserRepository;
-import com.easyjobs.domain.service.UserService;
 import com.easyjobs.exception.ResourceNotFoundException;
-import com.easyjobs.service.UserServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
